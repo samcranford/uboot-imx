27f9c1cd105295f0518439431d4046b289bfdaa7
June Tate-Gans <jtgans@google.com>
2019-08-20 16:21:00 -0500

    columbia: Add in initial board configs and devicetree
    Change-Id: I859374f0dd976c78fed784b8c84612dcc858fc60

---
+++ board/freescale/imx8mm_columbia/board_id.h
@@ -0,0 +1,16 @@
+#ifndef __BOARD_ID_H__
+#define __BOARD_ID_H__
+#include <common.h>
+// Returns board id based on gpio pins state:
+// 0 - P0 boards and EVT boards with 3Gb Micron (MT53B768M32D4NQ-062) DDR
+// 1 - EVT boards with 1Gb Hynix DDR
+// 2 - EVT boards with 1Gb Micron DDR
+// 3 - Invalid
+// 4 - DVT boards with 3Gb Micron (MT53B768M32D4NQ-062) DDR
+// 5 - DVT boards with 1Gb Hynix DDR
+// 6 - DVT boards with 1Gb Micron DDR
+// 7 - DVT boards with 3Gb Micron (MT53E768M32D4DT) DDR
+int get_board_id(void);
+int get_baseboard_id(void);
+size_t get_ddr_size(void);
+#endif
