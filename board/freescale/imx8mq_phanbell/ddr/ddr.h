b1cb6840b56c21ac71d8a6ff575bdf2217c5d7ed
Kai Yick <kingkai@google.com>
2019-10-07 23:04:19 -0700

    Update ddr.h to reflect function name changes.
    Change-Id: Ia647bf036f9a0fbb1188f73e0c91e2778bec9db5

--- board/freescale/imx8mq_phanbell/ddr/ddr.h
+++ board/freescale/imx8mq_phanbell/ddr/ddr.h
@@ -21,9 +21,7 @@ enum fw_type {
 void ddr_init_micron_3gb(void);
 void lpddr4_800M_cfg_phy(void);
 void ddr_init_micron_1gb(void);
-
-void ddr_init_hynix_1gb_evt(void);
-void ddr_init_hynix_1gb_dvt(void);
+void ddr_init_kingston_2gb(void);
 
 void ddr_load_train_code(enum fw_type type);
 void wait_ddrphy_training_complete(void);
